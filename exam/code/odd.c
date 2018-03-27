75627d721308505bb1a696a2082783fa700ea212
arunajay <arunajay1012@gmail.com>
2018-03-27 15:56:57 +0530

    Create odd.c

---
+++ exam/code/odd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main(void) {
+		int i;
+	printf("enter the number");
+	if(i%2==0)
+	{
+		printf("it is even number");
+	}
+	else{
+		printf("it is odd number");
+	}
+	return 0;
+}
